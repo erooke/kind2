92676e2fa1df95af6defed5a49b5207d1806eea0
Ethan Rooke <ethan@roo.ke>
2024-10-03 19:05:01 -0500

    nix development environment

--- flake.nix
+++ flake.nix
@@ -5,34 +5,38 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    ,
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      formatter.${system} = pkgs.alejandra;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    formatter.${system} = pkgs.alejandra;
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          ocamlPackages.ocaml
-          ocamlPackages.dune_3
-          ocamlPackages.findlib
-          ocamlPackages.dune-build-info
-          ocamlPackages.menhir
-          ocamlPackages.menhirLib
-          ocamlPackages.num
-          ocamlPackages.odoc
-          ocamlPackages.ounit
-          ocamlPackages.yojson
-          ocamlPackages.zmq
-          czmq
-          z3
-        ];
-      };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.ocamlPackages.ocaml
+        pkgs.ocamlPackages.dune_3
+        pkgs.ocamlPackages.findlib
+        pkgs.ocamlPackages.dune-build-info
+        pkgs.ocamlPackages.menhir
+        pkgs.ocamlPackages.menhirLib
+        pkgs.ocamlPackages.num
+        pkgs.ocamlPackages.odoc
+        pkgs.ocamlPackages.ounit
+        pkgs.ocamlPackages.yojson
+        pkgs.ocamlPackages.zmq
+        pkgs.ocamlPackages.ocaml-lsp
+        pkgs.ocamlPackages.ocamlformat
+        pkgs.ocamlPackages.utop
+        pkgs.czmq
+        pkgs.z3
+      ];
+
+      shellHook = ''
+        PATH="$(pwd)/_build/install/default/bin:$PATH"
+      '';
     };
+  };
 }
