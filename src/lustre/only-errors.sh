432cc77db105a3485da252a91c6b2c711a7bd736
Ethan Rooke <ethan@roo.ke>
2024-11-21 07:59:36 -0600

    src/lustre/only-errors.sh: quote to avoid globbing

--- src/lustre/only-errors.sh
+++ src/lustre/only-errors.sh
@@ -2,10 +2,10 @@
 # Run command passed as argument and print stderr output
 # only if the command failed to execute
 tempfile=$(mktemp)
-$@ 2> $tempfile
+$@ 2> "$tempfile"
 status=$?
 if [ $status -ne 0 ]; then
-  cat $tempfile 1>&2
+  cat "$tempfile" 1>&2
 fi
-rm $tempfile
+rm "$tempfile"
 exit $status
